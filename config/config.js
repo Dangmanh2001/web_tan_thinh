27b683cd8d25b1c4397e1421db2b02d09ba37819
Manh Dang <phaodai2000@gmail.com>
2026-03-25 21:37:08 +0700

    fix: production config use DB_NAME env var

--- config/config.js
+++ config/config.js
@@ -18,7 +18,7 @@ module.exports = {
   production: {
     username: process.env.DB_USERNAME || "root",
     password: process.env.DB_PASSWORD || null,
-    database: process.env.DB_PROD || "database_production",
+    database: process.env.DB_NAME || "database_production",
     host: process.env.DB_HOST || "127.0.0.1",
     dialect: "mysql",
   },
